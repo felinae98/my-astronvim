0da911367f83fe510b0a761c8c2fe73e9261cd1b
felinae98 <731499577@qq.com>
2023-08-29 13:42:04 +0800

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,3 +1,4 @@
+local utils = require "astronvim.utils"
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
@@ -6,7 +7,45 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.go" },
+  -- { import = "astrocommunity.pack.go" },
+  -- Golang support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "go", "gomod", "gosum", "gowork")
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed =
+        utils.list_insert_unique(opts.ensure_installed, { "gomodifytags", "iferr", "impl", "goimports", "golangci-lint" })
+
+      if not opts.handlers then opts.handlers = {} end
+      opts.handlers.gofmt = function()
+        local null_ls = require "null-ls"
+        null_ls.register(null_ls.builtins.formatting.gofmt)
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "gopls") end,
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "delve") end,
+      },
+    },
+    opts = {},
+  },
   {
     "ray-x/go.nvim",
     dependencies = {
@@ -15,7 +54,12 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "theHamsta/nvim-dap-virtual-text",
     },
+    opts = {},
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()',
   },
+
 
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.helm" },
