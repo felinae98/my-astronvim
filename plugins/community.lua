454a8119096e0415eef99a91eec93dc0a14be9ef
felinae98 <731499577@qq.com>
2024-01-10 16:42:06 +0800

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,4 +1,15 @@
 local utils = require "astronvim.utils"
+
+function remove_from_list(list, value)
+  for i = #list, 1, -1 do
+    if list[i] == value then
+      table.remove(list, i)
+    end
+  end
+
+  return list
+end
+
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
@@ -7,51 +18,13 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  -- { import = "astrocommunity.pack.go" },
-  -- Golang support
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "go", "gomod", "gosum", "gowork")
-      end
-    end,
-  },
+  { import = "astrocommunity.pack.go" },
   {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
       opts.ensure_installed =
-        utils.list_insert_unique(opts.ensure_installed, { "gomodifytags", "iferr", "impl", "goimports", "golangci-lint" })
+        remove_from_list(opts.ensure_installed, "gofumpt")
     end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "gopls") end,
-  },
-  {
-    "leoluz/nvim-dap-go",
-    ft = "go",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      {
-        "jay-babu/mason-nvim-dap.nvim",
-        opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "delve") end,
-      },
-    },
-    opts = {},
-  },
-  {
-    "ray-x/go.nvim",
-    dependencies = {
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-      "theHamsta/nvim-dap-virtual-text",
-    },
-    opts = {},
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()',
   },
 
   { import = "astrocommunity.pack.python" },
